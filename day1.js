4bd0827c56c2a9a2b7bdd68a9488cbb87794847c
Rodrigo Araújo Passos <araujo.rodrigo.passos@gmail.com>
2024-09-07 16:57:36 -0300

    Day 1 Challenge Completed

---
+++ day1.js
@@ -0,0 +1,14 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+console.log(numeroUm == stringUm)
+console.log(numeroDez == stringDez)
+console.log(numeroTrinta == stringTrinta)
+
+console.log(numeroUm === stringUm)
+console.log(numeroDez === stringDez)
+console.log(numeroTrinta === stringTrinta)
